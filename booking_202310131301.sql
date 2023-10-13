b19432d266e31363e89634686fe84d3ebed226dc
Korolev Ilya <113302248+Kiyoakiii@users.noreply.github.com>
2023-10-13 13:07:45 +0300

    Add files via upload

---
+++ booking_202310131301.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.booking (id_client,id_tour) VALUES
+	 (1,1),
+	 (1,2),
+	 (2,5),
+	 (3,4),
+	 (4,5),
+	 (2,3),
+	 (4,5),
+	 (3,3),
+	 (4,1),
+	 (1,2);
+INSERT INTO public.booking (id_client,id_tour) VALUES
+	 (2,3),
+	 (6,7),
+	 (7,5),
+	 (8,8),
+	 (9,9),
+	 (10,1);
